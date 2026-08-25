b1d236687bc85c418c46b6e03df9644ed0e1d198
Alex Silva <alexooodev@gmail.com>
2026-08-25 17:25:42 -0400

    project scaffolding

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+
+  production: false,
+
+  msalConfig: {
+
+    clientId: 'TU_APPLICATION_CLIENT_ID',
+
+    authority: 'https://login.microsoftonline.com/TU_TENANT_ID',
+
+    redirectUri: 'http://localhost:4200'
+
+  }
+
+};
